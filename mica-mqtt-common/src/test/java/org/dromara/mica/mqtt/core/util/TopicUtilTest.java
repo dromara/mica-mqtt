c894149b19dd2417ec603ea47eb7affa7517fd78
如梦技术 <596392912@qq.com>
2025-03-04 21:29:29 +0800

    :sparkles: 优化代码

--- mica-mqtt-common/src/test/java/org/dromara/mica/mqtt/core/util/TopicUtilTest.java
+++ mica-mqtt-common/src/test/java/org/dromara/mica/mqtt/core/util/TopicUtilTest.java
@@ -66,16 +66,16 @@ class TopicUtilTest {
 		String s1 = "$SYS/brokers/${node}/clients/${clientId}/disconnected";
 		String s2 = "$SYS/brokers/+/clients/+/disconnected";
 		String s3 = TopicUtil.getTopicFilter(s1);
-		Assertions.assertEquals(s3, s2);
+		Assertions.assertEquals(s2, s3);
 		s1 = "$SYS/brokers/${node}/clients/${clientId}abc/disconnected";
 		s3 = TopicUtil.getTopicFilter(s1);
-		Assertions.assertEquals(s3, s2);
+		Assertions.assertEquals(s2, s3);
 		s1 = "$SYS/brokers/${node}/clients/${clientId}abc${x}/disconnected";
 		s3 = TopicUtil.getTopicFilter(s1);
-		Assertions.assertEquals(s3, s2);
+		Assertions.assertEquals(s2, s3);
 		s1 = "$SYS/brokers/${node}/clients/abc${clientId}abc${x}123/disconnected";
 		s3 = TopicUtil.getTopicFilter(s1);
-		Assertions.assertEquals(s3, s2);
+		Assertions.assertEquals(s2, s3);
 	}
 
 	@Test
