8e9b6f7f305ba631a1f0f030daf3c3ef85b7c32c
如梦技术 <596392912@qq.com>
2025-05-20 08:51:57 +0800

    :wrench: 更新 module-info.java

--- mica-mqtt-common/src/main/moditect/module-info.java
+++ mica-mqtt-common/src/main/moditect/module-info.java
@@ -2,6 +2,8 @@ open module org.dromara.mica.mqtt.common {
 	requires transitive net.dreamlu.mica.net.core;
 	requires transitive org.dromara.mica.mqtt.codec;
 	exports org.dromara.mica.mqtt.core.common;
+	exports org.dromara.mica.mqtt.core.deserialize;
+	exports org.dromara.mica.mqtt.core.serializer;
 	exports org.dromara.mica.mqtt.core.util;
 	exports org.dromara.mica.mqtt.core.util.timer;
 }
