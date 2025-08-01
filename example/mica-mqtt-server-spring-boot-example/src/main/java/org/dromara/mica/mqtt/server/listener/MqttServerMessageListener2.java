385dd1f4f63da1f6ded7c31bb918b4c5f7ff18ab
如梦技术 <596392912@qq.com>
2025-08-01 16:49:41 +0800

    feat(example): 增加 MQTT 消息处理函数参数说明和客户端节点信息日志

--- example/mica-mqtt-server-spring-boot-example/src/main/java/org/dromara/mica/mqtt/server/listener/MqttServerMessageListener2.java
+++ example/mica-mqtt-server-spring-boot-example/src/main/java/org/dromara/mica/mqtt/server/listener/MqttServerMessageListener2.java
@@ -6,6 +6,7 @@ import org.dromara.mica.mqtt.server.pojo.User;
 import org.dromara.mica.mqtt.spring.server.MqttServerFunction;
 import org.springframework.stereotype.Service;
 import org.tio.core.ChannelContext;
+import org.tio.core.Node;
 
 /**
  * 消息监听器示例2，MqttServerFunction 注解订阅，注意：如果自行实现了 IMqttMessageListener，MqttServerFunction 注解就不生效了。
@@ -20,7 +21,7 @@ public class MqttServerMessageListener2 {
 	 * MQTT消息处理函数
 	 *
 	 * @param topic mqtt Topic
-	 * @param user 订阅消息的负载内容，默认 json 序列化
+	 * @param user  订阅消息的负载内容，默认 json 序列化
 	 */
 	@MqttServerFunction("/test/object")
 	public void func1(String topic, User<?> user) {
@@ -33,17 +34,19 @@ public class MqttServerMessageListener2 {
 	}
 
 	/**
-	 * MQTT消息处理函数，/test/+
+	 * MQTT消息处理函数，匹配 mqtt Topic /test/+，如何需要匹配所以消息，请使用通配符 #
 	 *
-	 * @param context ChannelContext，可选参数
-	 * @param topic 实际接收到消息的主题名称，可选参数
+	 * @param context        ChannelContext，可选参数
+	 * @param topic          实际接收到消息的主题名称，可选参数
 	 * @param publishMessage 完整的MQTT发布消息对象，包含消息头和负载，可选参数
-	 * @param message 消息负载内容，以字节数组形式提供，可选参数，也可支持对象形式，默认 json 序列化
+	 * @param message        消息负载内容，以字节数组形式提供，可选参数，也可支持对象形式，默认 json 序列化
 	 */
 	@MqttServerFunction("/test/${xxxx}")
 	public void func3(ChannelContext context, String topic, MqttPublishMessage publishMessage, byte[] message) {
+		// 获取客户端节点信息
+		Node clientNode = context.getClientNode();
 		// 记录接收到的MQTT消息信息
-		log.info("topic:{} message:{}", topic, new String(message));
+		log.info("clientNode:{} topic:{} publishMessage:{} message:{}", clientNode, topic, publishMessage, new String(message));
 	}
 
 }
