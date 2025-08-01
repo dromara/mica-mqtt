d6e9ed36483eb5e87c972af4333527dd3e2c2c3d
如梦技术 <596392912@qq.com>
2025-08-01 18:13:37 +0800

    feat(mqtt-codec): 更新模块信息，导出新包
    - 在 module-info.java 中导出了 org.dromara.mica.mqtt.codec.codes 和 org.dromara.mica.mqtt.codec.exception 包
    - 这些新导出的包可能包含了 MQTT 编解码相关的新功能或错误处理代码

--- mica-mqtt-codec/src/main/moditect/module-info.java
+++ mica-mqtt-codec/src/main/moditect/module-info.java
@@ -1,5 +1,7 @@
 open module org.dromara.mica.mqtt.codec {
 	requires net.dreamlu.mica.net.core;
 	exports org.dromara.mica.mqtt.codec;
+	exports org.dromara.mica.mqtt.codec.codes;
+	exports org.dromara.mica.mqtt.codec.exception;
 	exports org.dromara.mica.mqtt.codec.properties;
 }
