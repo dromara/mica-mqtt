2e055a65549a61f6b084ed47acdd512471a69cb5
如梦技术 <596392912@qq.com>
2025-09-20 13:56:20 +0800

    :sparkles: 添加 TrieTopicManagerTest

--- mica-mqtt-server/src/test/java/org/dromara/mica/mqtt/core/server/test/TrieTopicManagerTest.java
+++ mica-mqtt-server/src/test/java/org/dromara/mica/mqtt/core/server/test/TrieTopicManagerTest.java
@@ -1,10 +1,13 @@
 package org.dromara.mica.mqtt.core.server.test;
 
+import org.dromara.mica.mqtt.core.server.model.Subscribe;
 import org.dromara.mica.mqtt.core.server.session.TrieTopicManager;
-import org.tio.utils.thread.ThreadUtils;
+import org.dromara.mica.mqtt.core.util.TopicUtil;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.TimeUnit;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * TrieTopicManager 测试
@@ -13,103 +16,163 @@ import java.util.concurrent.TimeUnit;
  */
 class TrieTopicManagerTest {
 
-	public static TrieTopicManager test1() {
+	/**
+	 * 方便测试
+	 *
+	 * @param topicFilter topicFilter
+	 * @param topicName   topicName
+	 * @return 是否匹配
+	 */
+	private static boolean match(String topicFilter, String topicName) {
 		TrieTopicManager topicManager = new TrieTopicManager();
-		topicManager.addSubscribe("/sys/1/456/thing/model/down_raw", "client1", 1);
-		topicManager.addSubscribe("/sys/2/456/thing/model/down_raw", "client1", 1);
-		topicManager.addSubscribe("/sys/3/4567/thing/model/down_raw", "client1", 1);
-		topicManager.addSubscribe("/sys/4/45678/thing/model/down_raw", "client1", 1);
-		topicManager.addSubscribe("/sys/1/4561/thing/model/down_raw", "client1", 0);
-		topicManager.addSubscribe("/sys/2/45612/thing/model/down_raw", "client1", 1);
-		topicManager.addSubscribe("/sys/+/+/thing/model/down_raw", "client1", 0);
-		topicManager.addSubscribe("/sys/3/456/thing/model/down_raw", "client1", 1);
-		topicManager.addSubscribe("/sys/12/456/thing/model/down_raw", "client1", 1);
-		topicManager.addSubscribe("/sys/11/4567/thing/model/down_raw", "client1", 1);
-		topicManager.addSubscribe("/sys/111/45678/thing/model/down_raw", "client1", 1);
-		topicManager.addSubscribe("/sys/123/4561/thing/model/down_raw", "client1", 0);
-		topicManager.addSubscribe("/sys/123/45612/thing/model/down_raw", "client1", 1);
-		topicManager.addSubscribe("/sys/1/+/thing/model/down_raw", "client1", 0);
-		topicManager.addSubscribe("/sys/1/456/thing/model/down_raw", "client2", 1);
-		topicManager.addSubscribe("/sys/2/456/thing/model/down_raw", "client2", 1);
-		topicManager.addSubscribe("/sys/3/4567/thing/model/down_raw", "client2", 1);
-		topicManager.addSubscribe("/sys/4/45678/thing/model/down_raw", "client2", 1);
-		topicManager.addSubscribe("/sys/1/4561/thing/model/down_raw", "client2", 0);
-		topicManager.addSubscribe("/sys/2/45612/thing/model/down_raw", "client2", 1);
-		topicManager.addSubscribe("/sys/+/+/thing/model/down_raw", "client2", 0);
-		topicManager.addSubscribe("/sys/3/456/thing/model/down_raw", "client2", 1);
-		topicManager.addSubscribe("/sys/12/456/thing/model/down_raw", "client2", 1);
-		topicManager.addSubscribe("/sys/11/4567/thing/model/down_raw", "client2", 1);
-		topicManager.addSubscribe("/sys/111/45678/thing/model/down_raw", "client2", 1);
-		topicManager.addSubscribe("/sys/123/4561/thing/model/down_raw", "client2", 0);
-		topicManager.addSubscribe("/sys/123/45612/thing/model/down_raw", "client2", 1);
-		topicManager.addSubscribe("/sys/1/+/thing/model/down_raw", "client2", 0);
-		topicManager.addSubscribe("/sys/1/456/thing/model/down_raw", "client3", 1);
-		topicManager.addSubscribe("/sys/2/456/thing/model/down_raw", "client3", 1);
-		topicManager.addSubscribe("/sys/3/4567/thing/model/down_raw", "client3", 1);
-		topicManager.addSubscribe("/sys/4/45678/thing/model/down_raw", "client3", 1);
-		topicManager.addSubscribe("/sys/1/4561/thing/model/down_raw", "client3", 0);
-		topicManager.addSubscribe("/sys/2/45612/thing/model/down_raw", "client3", 1);
-		topicManager.addSubscribe("/sys/+/+/thing/model/down_raw", "client3", 0);
-		topicManager.addSubscribe("/sys/3/456/thing/model/down_raw", "client3", 1);
-		topicManager.addSubscribe("/sys/12/456/thing/model/down_raw", "client3", 1);
-		topicManager.addSubscribe("/sys/11/4567/thing/model/down_raw", "client3", 1);
-		topicManager.addSubscribe("/sys/111/45678/thing/model/down_raw", "client3", 1);
-		topicManager.addSubscribe("/sys/123/4561/thing/model/down_raw", "client3", 0);
-		topicManager.addSubscribe("/sys/123/45612/thing/model/down_raw", "client3", 1);
-		topicManager.addSubscribe("/sys/1/+/thing/model/down_raw", "client3", 0);
-		topicManager.addSubscribe("$share/group1/sys/123/456/thing/model/down_raw", "client1", 0);
-		topicManager.addSubscribe("$queue/sys/123/456/thing/model/down_raw", "client31", 0);
-		topicManager.addSubscribe("$share/group1/sys/123/456/thing/model/down_raw", "client2", 0);
-		topicManager.addSubscribe("$queue/sys/123/456/thing/model/down_raw", "client2", 0);
-		topicManager.addSubscribe("$share/group1/sys/123/456/thing/model/down_raw", "client3", 0);
-		long startTime = System.nanoTime();
-		for (int i = 0; i < 100000; i++) {
-			topicManager.searchSubscribe("/sys/123/456/thing/model/down_raw");
-		}
-		long costTime = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startTime);
-		System.out.println("test1\t" + costTime);
-		return topicManager;
+		return match(topicManager, topicFilter, topicName);
 	}
 
-	public static void testTime() {
-		for (int i = 0; i < 100; i++) {
-			test1();
-		}
+	/**
+	 * 方便测试
+	 *
+	 * @param topicFilter topicFilter
+	 * @param topicName   topicName
+	 * @return 是否匹配
+	 */
+	private static boolean match(TrieTopicManager topicManager, String topicFilter, String topicName) {
+		String clientId = "client1";
+		int qos = 0;
+		topicManager.addSubscribe(topicFilter, clientId, qos);
+		List<Subscribe> subscribeList = topicManager.searchSubscribe(topicName);
+		return subscribeList.stream().anyMatch(subscribe -> {
+			return subscribe.getClientId().equals(clientId) && subscribe.getMqttQoS() == qos;
+		});
 	}
 
-	public static void testMem() {
+	@Test
+	void testGetSubscriptions() {
 		TrieTopicManager topicManager = new TrieTopicManager();
-		long startTime = System.nanoTime();
-		for (int i = 0; i < 10000; i++) {
-			for (int j = 0; j < 100; j++) {
-				topicManager.addSubscribe("/sys/1/" + i + "/" + j, "client" + i, 1);
-			}
-		}
-		long costTime = TimeUnit.NANOSECONDS.toMillis(System.nanoTime() - startTime);
-		System.out.println("testMem\t" + costTime);
-		System.out.println(topicManager);
+		Assertions.assertTrue(match(topicManager, "123", "123"));
+		Assertions.assertTrue(match(topicManager, "/1234/", "/1234/"));
+		Assertions.assertTrue(match(topicManager, "$queue/12345", "12345"));
+		Assertions.assertTrue(match(topicManager, "$queue//123456", "/123456"));
+		Assertions.assertTrue(match(topicManager, "$share/test/1234567", "1234567"));
+		Assertions.assertTrue(match(topicManager, "$share/test//12345678/11/", "/12345678/11/"));
+		List<String> subscriptions = topicManager.getSubscriptions("client1").stream()
+			.map(Subscribe::getTopicFilter)
+			.collect(Collectors.toList());
+		Assertions.assertTrue(subscriptions.contains("123"));
+		Assertions.assertTrue(subscriptions.contains("/1234/"));
+		Assertions.assertTrue(subscriptions.contains("$queue/12345"));
+		Assertions.assertTrue(subscriptions.contains("$queue//123456"));
+		Assertions.assertTrue(subscriptions.contains("$share/test/1234567"));
+		Assertions.assertTrue(subscriptions.contains("$share/test//12345678/11/"));
 	}
 
-	public static void testConcurrent() {
+	@Test
+	void testMatch() {
+		// gitee issues #I56BTC /iot/test/# 无法匹配到 /iot/test 和 /iot/test/
+		Assertions.assertFalse(match("+", "/iot/test"));
+		Assertions.assertFalse(match("+", "iot/test"));
+		Assertions.assertFalse(match("+", "/iot/test"));
+		Assertions.assertFalse(match("+", "/iot"));
+		Assertions.assertFalse(match("+/test", "/iot/test"));
+		Assertions.assertFalse(match("/iot/test/+/", "/iot/test/123"));
+
+		Assertions.assertTrue(match("/iot/test/+", "/iot/test/123"));
+		Assertions.assertFalse(match("/iot/test/+", "/iot/test/123/"));
+		Assertions.assertTrue(match("/iot/+/test", "/iot/abc/test"));
+		Assertions.assertFalse(match("/iot/+/test", "/iot/abc/test/"));
+		Assertions.assertFalse(match("/iot/+/test", "/iot/abc/test1"));
+		Assertions.assertTrue(match("/iot/+/+/test", "/iot/abc/123/test"));
+		Assertions.assertFalse(match("/iot/+/+/test", "/iot/abc/123/test1"));
+		Assertions.assertFalse(match("/iot/+/+/test", "/iot/abc/123/test/"));
+		Assertions.assertTrue(match("/iot/+/+/+", "/iot/abc/123/test"));
+		Assertions.assertFalse(match("/iot/+/+/+", "/iot/abc/123/test/"));
+		Assertions.assertTrue(match("/iot/+/test", "/iot/a/test"));
+		Assertions.assertTrue(match("/iot/+/test", "/iot/a/test"));
+		Assertions.assertFalse(match("/iot/+/+/+", "/iot/a//test/"));
+		Assertions.assertFalse(match("/iot/+/+/+", "/iot/a/b/c/"));
+		Assertions.assertFalse(match("/iot/+/+/+", "/iot/a"));
+		Assertions.assertFalse(TopicUtil.match("/iot/test/+", "/iot/test"));
+
+		Assertions.assertTrue(match("#", "/iot/test"));
+		Assertions.assertTrue(match("/iot/test/#", "/iot/test"));
+		Assertions.assertTrue(match("/iot/test/#", "/iot/test/"));
+		Assertions.assertTrue(match("/iot/test/#", "/iot/test/1"));
+		Assertions.assertTrue(match("/iot/test/#", "/iot/test/123123/12312"));
+
+		Assertions.assertTrue(match("/iot/test/123", "/iot/test/123"));
+	}
+
+	@Test
+	void testMatchGroup() {
+		Assertions.assertTrue(match("$queue/123", "123"));
+		Assertions.assertFalse(match("$queue/123", "/123"));
+		Assertions.assertFalse(match("$queue//123", "123"));
+		Assertions.assertTrue(match("$queue//123", "/123"));
+
+		Assertions.assertTrue(match("$share/test/123", "123"));
+		Assertions.assertFalse(match("$share/test/123", "/123"));
+		Assertions.assertFalse(match("$share/test//123", "123"));
+		Assertions.assertTrue(match("$share/test//123", "/123"));
+	}
+
+	@Test
+	void testAdd() {
 		TrieTopicManager topicManager = new TrieTopicManager();
-		ExecutorService executor = ThreadUtils.getBizExecutor(10);
-		for (int i = 0; i < 10000; i++) {
-			for (int j = 0; j < 10; j++) {
-				String topic = "/sys/1/" + i + "/" + j;
-				String client = "client" + i;
-				executor.execute(() -> {
-					topicManager.addSubscribe(topic, client, 2);
-				});
-			}
-		}
-		ThreadUtils.sleep(8000);
-		System.out.println(topicManager);
+		topicManager.addSubscribe("test/+", "client1", 0);
+		topicManager.addSubscribe("test/123", "client1", 1);
+		topicManager.addSubscribe("$queue/test/123", "client1", 2);
+		topicManager.addSubscribe("$share/group1/test/123", "client1", 1);
+		List<Subscribe> subscribeList = topicManager.getSubscriptions("client1");
+		Assertions.assertEquals(4, subscribeList.size());
 	}
 
-	public static void main(String[] args) {
-		testTime();
-		testMem();
-		testConcurrent();
+	@Test
+	void testRemove() {
+		TrieTopicManager topicManager = new TrieTopicManager();
+		topicManager.addSubscribe("test/123", "client1", 0);
+		topicManager.addSubscribe("$queue/test/123", "client1", 0);
+		topicManager.addSubscribe("$share/group1/test/123", "client1", 0);
+		topicManager.removeSubscribe("$queue/test/123", "client1");
+		List<Subscribe> subscribeList = topicManager.getSubscriptions("client1");
+		Assertions.assertEquals(2, subscribeList.size());
+	}
+
+	@Test
+	void testSearch() {
+		TrieTopicManager topicManager = new TrieTopicManager();
+		topicManager.addSubscribe("test/+", "client1", 0);
+		topicManager.addSubscribe("test/+/", "client2", 1);
+		topicManager.addSubscribe("test/+/1", "client3", 1);
+		topicManager.addSubscribe("$queue/test/#", "client4", 0);
+		topicManager.addSubscribe("$share/group1/test/123", "client5", 0);
+		List<Subscribe> subscribeList = topicManager.getSubscriptions("client1");
+		Assertions.assertEquals(1, subscribeList.size());
+		List<Subscribe> subscribes = topicManager.searchSubscribe("test/123");
+		System.out.println(subscribes);
+	}
+
+	@Test
+	void test() {
+		TrieTopicManager topicManager = new TrieTopicManager();
+		topicManager.addSubscribe("test/123", "client1", 1);
+		topicManager.addSubscribe("test/1234", "client1", 1);
+		topicManager.addSubscribe("test/1235", "client1", 1);
+		topicManager.addSubscribe("test1/123", "client1", 1);
+		topicManager.addSubscribe("+/123", "client1", 0);
+		topicManager.addSubscribe("test/#", "client1", 1);
+		topicManager.addSubscribe("/test/123", "client1", 0);
+		topicManager.addSubscribe("$share/group1/test/123", "client2", 0);
+		topicManager.addSubscribe("$queue/test/123", "client3", 0);
+
+		List<Subscribe> subscribeList = topicManager.searchSubscribe("test/123");
+		Assertions.assertFalse(subscribeList.isEmpty());
+
+		List<Subscribe> subscriptions = topicManager.getSubscriptions("client1");
+		Assertions.assertFalse(subscriptions.isEmpty());
+
+		topicManager.removeSubscribe("/test/123", "client1");
+		topicManager.removeSubscribe("client1");
+		subscriptions = topicManager.getSubscriptions("client1");
+		Assertions.assertTrue(subscriptions.isEmpty());
 	}
 
 }
