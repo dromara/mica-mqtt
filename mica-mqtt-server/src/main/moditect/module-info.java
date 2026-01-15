5ac07affaf962d565853b9545a66e5e7afd8fa7e
如梦技术 <596392912@qq.com>
2026-01-15 14:30:57 +0800

    refactor(mica-mqtt-server): 调整模块导出结构
    - 移除 org.dromara.mica.mqtt.core.server.broker 和 cluster 以及 dispatcher 导出
    - 添加 org.dromara.mica.mqtt.core.server.pipeline 及其子包的导出
    - 优化模块的包导出组织结构

--- mica-mqtt-server/src/main/moditect/module-info.java
+++ mica-mqtt-server/src/main/moditect/module-info.java
@@ -5,15 +5,15 @@ open module org.dromara.mica.mqtt.server {
 
 	exports org.dromara.mica.mqtt.core.server;
 	exports org.dromara.mica.mqtt.core.server.auth;
-	exports org.dromara.mica.mqtt.core.server.broker;
-	exports org.dromara.mica.mqtt.core.server.cluster;
-	exports org.dromara.mica.mqtt.core.server.dispatcher;
 	exports org.dromara.mica.mqtt.core.server.enums;
 	exports org.dromara.mica.mqtt.core.server.event;
 	exports org.dromara.mica.mqtt.core.server.http.handler;
 	exports org.dromara.mica.mqtt.core.server.interceptor;
 	exports org.dromara.mica.mqtt.core.server.listener;
 	exports org.dromara.mica.mqtt.core.server.model;
+	exports org.dromara.mica.mqtt.core.server.pipeline;
+	exports org.dromara.mica.mqtt.core.server.pipeline.handler;
+	exports org.dromara.mica.mqtt.core.server.pipeline.message;
 	exports org.dromara.mica.mqtt.core.server.protocol;
 	exports org.dromara.mica.mqtt.core.server.serializer;
 	exports org.dromara.mica.mqtt.core.server.session;
